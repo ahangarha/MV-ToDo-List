8789f5ed4fd691627928847b311e20f21adfeab9
Mostafa Ahangarha <ahangarha@riseup.net>
2022-02-15 16:30:55 +0300

    populate tasks from js to html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+import TodoList from './modules/todoList.js';
 import('./style.css');
+
+const todoListWrapper = document.getElementById('todo-list');
+
+const todos = [
+  {
+    index: 2,
+    description: 'finish this task',
+    completed: false,
+  },
+  {
+    index: 1,
+    description: 'Implement completed style',
+    completed: true,
+  },
+];
+
+const todoList = new TodoList(todoListWrapper, todos);
---
+++ src/modules/todoList.js
@@ -0,0 +1,53 @@
+export default class TodoList {
+  constructor(wrapper, todos = []) {
+    this.wrapper = wrapper;
+    this.todos = todos;
+    this.init();
+  }
+
+  init() {
+    this.todos.forEach((todo) => {
+      this.addTodo(todo);
+    });
+  }
+
+  addTodo(todo) {
+    const li = document.createElement('li');
+    const label = document.createElement('label');
+    const chkbox = document.createElement('input');
+
+    chkbox.setAttribute('type', 'checkbox');
+
+    if (todo.completed) {
+      chkbox.setAttribute('checked', 'checked');
+      label.classList.add('checked');
+    }
+
+    label.appendChild(chkbox);
+    label.innerHTML += todo.description;
+
+    const dragBtn = document.createElement('button');
+    dragBtn.setAttribute('type', 'button');
+    dragBtn.innerHTML = `
+    <button type="button">
+      <svg
+        class="icon"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+        ></path>
+      </svg>
+    </button>`;
+
+    li.appendChild(label);
+    li.appendChild(dragBtn);
+    this.wrapper.appendChild(li);
+  }
+}

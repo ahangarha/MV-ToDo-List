8578593799a0e573941ce09d5c0b072a81781e7c
Mostafa Ahangarha <ahangarha@riseup.net>
2022-02-16 01:35:25 +0300

    redesign todo list items

--- src/modules/todoList.js
+++ src/modules/todoList.js
@@ -38,41 +38,97 @@ export default class TodoList {
     const li = document.createElement('li');
     li.classList.add('todo');
 
-    const label = document.createElement('label');
-    const chkbox = document.createElement('input');
+    const completionIcon = document.createElement('button');
+    completionIcon.setAttribute('type', 'button');
+    completionIcon.classList.add('completionIcon', 'icon');
+    completionIcon.innerHTML = `
+    <svg
+      class="icon"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
+      ></path>
+    </svg>
+    <svg
+      class="icon"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
+      ></path>
+    </svg>
+    `;
 
-    chkbox.setAttribute('type', 'checkbox');
+    const description = document.createElement('input');
+    description.setAttribute('type', 'text');
+    description.setAttribute('name', 'description');
+    description.setAttribute('disabled', '');
+    description.setAttribute('value', todo.description);
+    description.classList.add('description');
+
+    const actionIcon = document.createElement('div');
+    actionIcon.classList.add('actionIcon', 'icon');
+
+    const dragIcon = document.createElement('button');
+    dragIcon.setAttribute('type', 'button');
+    dragIcon.innerHTML = `
+    <svg
+      class="icon"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+      ></path>
+    </svg>
+    `;
+
+    const deleteIcon = document.createElement('button');
+    deleteIcon.setAttribute('type', 'button');
+    deleteIcon.innerHTML = `
+    <svg
+      class="icon"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+      ></path>
+    </svg>
+    `;
+
+    actionIcon.appendChild(dragIcon);
 
     if (todo.completed) {
-      chkbox.setAttribute('checked', 'checked');
-      label.classList.add('checked');
+      li.classList.add('completed');
     }
 
-    label.appendChild(chkbox);
-    label.innerHTML += todo.description;
-
-    const dragBtn = document.createElement('button');
-    dragBtn.setAttribute('type', 'button');
-    dragBtn.innerHTML = `
-    <button type="button">
-      <svg
-        class="icon"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
-        ></path>
-      </svg>
-    </button>`;
-
-    li.appendChild(label);
-    li.appendChild(dragBtn);
+    li.appendChild(completionIcon);
+    li.appendChild(description);
+    li.appendChild(actionIcon);
     this.wrapper.appendChild(li);
   }
 

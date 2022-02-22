7f3c2aef86510d0608ce419312d0b5b4178cd51e
brahim <brahimgb1team@gmail.com>
2022-02-22 12:54:47 +0000

    add new item

---
+++ src/modules/todoList.test.js
@@ -0,0 +1,22 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import TodoList from './todoList.js';
+
+describe('Test - Part 1', () => {
+  test('add new todo', () => {
+    //Arrange
+    document.body.innerHTML = '<ul id="todo-list"></ul>';
+    const wrapper = document.getElementById('todo-list');
+    const todoList = new TodoList(wrapper);
+    const todoTitle = 'first todo!';
+
+    // Act
+    todoList.addNewItem(todoTitle);
+
+    //Assert
+    expect(todoList.todos.length).toBe(1);
+    expect(wrapper.innerHTML).not.toBe("");
+  });
+});

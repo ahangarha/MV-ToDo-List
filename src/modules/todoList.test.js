71fbedbc99a65b400fad2ad21cb35baced1f033f
Mostafa Ahangarha <ahangarha@riseup.net>
2022-03-17 20:58:45 +0300

    add test for adding multiple todos and remove one in the middle

--- src/modules/todoList.test.js
+++ src/modules/todoList.test.js
@@ -20,4 +20,19 @@ describe('Test TodoList class', () => {
     expect(todoIsRemoved).toBe(true);
     expect(todoList.todos.length).toBe(0);
   });
+
+  test('Add 3 todos and remove the second one', () => {
+    const todoList = new TodoList();
+    todoList.init();
+    const descriptions = ['one', 'two', 'three'];
+
+    descriptions.forEach((description) => {
+      todoList.addNewTodo(description);
+    });
+
+    todoList.removeTodo(1);
+
+    expect(todoList.todos[0].description).toBe('one');
+    expect(todoList.todos[1].description).toBe('three');
+  });
 });
